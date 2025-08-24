869c028dbec050b48418673cabb7b1518df2ee55
Jonathan Lansey <lansey+github@gmail.com>
2025-08-24 15:31:56 -0400

    first draft aarhous bingo sheets

---
+++ aarhus.py
@@ -0,0 +1,49 @@
+# Boston themed bingo words
+sorted_list = [
+    "Street Mattress", "Street Mattress with Graffitti", # mattresses
+    "Cinnamon on the Street", 
+    "Crocodile Graffiti", "Multi-Story Street Mural", "The Bird Graffiti", # art
+    "Swallows in Backyards", 
+    "Aarhus University Yellow Brick Building",
+    "ARoS", "Moesgaard", "Den Gamle By", # museums
+    "Møllestien", "Botanical Gardens", "Clock Tower", "Cathedral", "Salling Rooftop", # other attractions
+    "Domen", "Dokk1", "Hipster Hill", "Aarhus Teater", "Godsbanen", "View from Godsbanen", # other attractions
+    "Cafe Mellemfolk", # cafes
+    "Bus 2A", "Letbanen", # public transportation
+    "Hygge",
+    "Unlocked Bicycle",
+    "The Sea",
+    "Pet a Deer", "Have an Interaction in Danish", "Hear the Bell at Dokk1", # challenges
+    "Paper Christmas Star", "Paper Christmas Heart", "Christmas Market", # christmas
+    "Use an Old-Timey Phone", "Apothecary", "Horse-drawn Cart", # things in Den Gamle By
+    "The Boy", "Inside the Rainbow Panorama", # things in ARoS
+    "Winter Bathers", "Jogger", # people-spotting
+]
+
+constraints = [
+    {"Street Mattress", "Street Mattress with Graffitti"},  # mattresses
+    {"Dokk1", "Hear the Bell at Dokk1"}, # Dokk1
+    {"Godsnanen", "View from Godsbanen"} # Godsbanen
+]
+
+center_word = "Den Gamle By"
+
+# https://patorjk.com/software/taag/#p=display&f=Slant&t=Aarhus%20BINGO
+
+##ascii_art_header = """
+##  ___    ___  ______ _   _ _   _ _____  ______ _____ _   _ _____ _____ 
+## / _ \\  / _ \\ | ___ \\ | | | | | /  ___| | ___ \\_   _| \\ | |  __ \\  _  |
+##/ /_\\ \\/ /_\\ \\| |_/ / |_| | | | \\ `--.  | |_/ / | | |  \\| | |  \\/ | | |
+##|  _  ||  _  ||    /|  _  | | | |`--. \\ | ___ \\ | | | . ` | | __| | | |
+##| | | || | | || |\\ \\| | | | |_| /\\__/ / | |_/ /_| |_| |\\  | |_\\ \\ \\_/ /
+##\\_| |_/\\_| |_/\\_| \\_\\_| |_/\\___/\\____/  \\____/ \\___/\\_| \\_/\\____/\\___/ 
+##"""
+
+ascii_art_header = """
+    ___              __                  ____  _____   ____________ 
+   /   | ____ ______/ /_  __  _______   / __ )/  _/ | / / ____/ __ \\
+  / /| |/ __ `/ ___/ __ \\/ / / / ___/  / __  |/ //  |/ / / __/ / / /
+ / ___ / /_/ / /  / / / / /_/ (__  )  / /_/ // // /|  / /_/ / /_/ / 
+/_/  |_\\__,_/_/  /_/ /_/\\__,_/____/  /_____/___/_/ |_/\\____/\\____/  
+
+"""
